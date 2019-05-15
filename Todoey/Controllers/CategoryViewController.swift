20a14a100e427e58a1a408238c94a26598775a20
Juan <jmgiusti91@gmail.com>
2019-05-15 10:50:28 -0300

    CoreData completely works with two entities: Items and Category

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,110 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Juan Martin Giusti on 5/14/19.
+//  Copyright © 2019 Juan Martin Giusti. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categories = [Category]()
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadCategories()
+    }
+    
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        cell.textLabel?.text = categories[indexPath.row].name
+        return cell
+    }
+    
+    
+    //MARK: TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToItems" {
+            let toDoListVC = segue.destination as! TodoListViewController
+            if let indexPath = tableView.indexPathForSelectedRow {
+                toDoListVC.selectedCategory = categories[indexPath.row]
+            }
+        }
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    func saveCategories() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
+        
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+    
+    //MARK: - Add New Categories
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            
+            let category = Category(context: self.context)
+            
+            category.name = textField.text!
+            
+            self.categories.append(category)
+            
+            self.saveCategories()
+            
+            //self.tableView.reloadData()
+            
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Add New Category"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
+    
+}
--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,16 +7,26 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController {
 
     var itemArray = [Item]()
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    var selectedCategory: Category? {
+        didSet {
+            loadItems()
+        }
+    }
+    
+    //let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         loadItems()
         /*if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray += items
@@ -42,6 +52,12 @@ class TodoListViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        //Delete objetos de context
+        //context.delete(itemArray[indexPath.row])
+        //itemArray.remove(at: indexPath.row)
+        //context.save()
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         saveItems()
         tableView.reloadData()
@@ -60,9 +76,16 @@ class TodoListViewController: UITableViewController {
                 print("A name of item is mandatory")
                 return
             }*/
-            let item = Item()
+            
+            //let item - Item()
+            
+            let item = Item(context: self.context)
             
             item.title = textField.text!
+            
+            item.done = false
+            
+            item.parentCategory = self.selectedCategory
             
             self.itemArray.append(item)
             
@@ -85,24 +108,78 @@ class TodoListViewController: UITableViewController {
     }
     
     func saveItems() {
-        let encoder = PropertyListEncoder()
+        
+        //Encoder
+        /*let encoder = PropertyListEncoder()
         do {
             let data = try encoder.encode(itemArray)
             try data.write(to: dataFilePath!)
         } catch {
             print("Error encoding item array, \(error)")
+        }*/
+        
+        //Core Data
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
         }
+        
+        self.tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
+    //Item.fetchRequest es el valor por defecto cuando se invoca el metodo sin pasarle parametros.
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        /*if let data = try? Data(contentsOf: dataFilePath!) {
             let decoder = PropertyListDecoder()
             do {
                 itemArray = try decoder.decode([Item].self, from: data)
             } catch {
                 print("Error decoding item array, \(error)")
             }
+        }*/
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+        self.tableView.reloadData()
+    }
+}
+
+
+//MARK: -Search bar methods
+extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        //print(searchBar.text!)
+        
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count == 0 {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
         }
     }
+    
 }
 
